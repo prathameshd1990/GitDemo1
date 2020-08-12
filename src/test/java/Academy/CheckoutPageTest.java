78656015ca22d691fbdb29df437b18047ceaac45
Prathamesh Dhamanaskar <prathameshd1990@yahoo.in>
2020-08-12 07:49:33 +0530

    Asian Team

---
+++ src/test/java/Academy/CheckoutPageTest.java
@@ -0,0 +1,12 @@
+package Academy;
+
+import org.testng.annotations.Test;
+
+public class CheckoutPageTest {
+	
+	@Test
+	public void checkoutPageTitleTest() {
+		System.out.println("Checkout Page Title");
+	}
+
+}
